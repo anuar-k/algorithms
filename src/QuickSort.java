3c03109e8e9847ae010aba93d1e02f52d7d4f36a
Anuar <https://github.com/anuar-k/HTML_CSS_2>
2020-08-01 16:34:56 +0600

    changed quick sort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -6,7 +6,7 @@ public class QuickSort {
             System.out.print(arr[i] + " ");
         }
 
-        quickSort(0 , arr.length -1 );
+        quickSort(0, arr.length - 1);
 
         System.out.println();
         for (int i = 0; i < arr.length; i++) {
@@ -14,14 +14,11 @@ public class QuickSort {
         }
     }
 
-
     public static void quickSort(int left, int right) {
-
         int i = left;
         int j = right;
         int tmp;
         int cur = arr[(left + right) / 2];
-
 
         while (i <= j) {
             while ((arr[i] < cur) && (i < right)) i++;
@@ -34,10 +31,8 @@ public class QuickSort {
                 i++;
                 j--;
             }
-
         }
         if (i < right) quickSort(i, right);
         if (left < j) quickSort(left, j);
     }
-
 }
