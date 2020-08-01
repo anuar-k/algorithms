5a1a3b79baeeb823eed117d70bc97fbbd81d3835
Anuar <https://github.com/anuar-k/HTML_CSS_2>
2020-08-01 15:45:07 +0600

    added bubblesort

---
+++ src/BubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        //Создаем массив
+        int[] arr = {10, 1, 0, 4, 2, 9};
+
+        //выводим неотсотированный массив
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        System.out.println();
+        //выводим отсотированный массив
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
